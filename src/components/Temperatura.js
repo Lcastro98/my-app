99bdd5420c5221d88fe2abd828d42d15a83cf507
Andrea Lorena Castro Manzano <lcastro0398@gmail.com>
2022-04-30 18:21:23 -0500

    temperatura
    Componente que con un operador ternearo determina si está haciendo calor o frÃío

---
+++ src/components/Temperatura.js
@@ -0,0 +1,19 @@
+import React, {Fragment} from 'react';
+
+const Temperatura = () => {
+
+    const temperatura = 21;
+    return ( 
+        <Fragment>
+            <h2>Frío o calor?</h2>
+
+            <h4>
+                {
+                    temperatura > 20 ? 'Calor' : 'Frío'
+                }
+            </h4>
+        </Fragment>
+    );
+}
+ 
+export default Temperatura;
